d60de6304cea13d5eb62e5162a33b5ef56f3d04a
Gennady Borisenko <gennady.borisenko@profitclicks.ru>
2020-04-17 14:04:47 +0300

    поправил main

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"flag"
 	"github.com/Aneg/calendar/internal/config"
-	"github.com/Aneg/calendar/internal/repositories/memory"
 	"github.com/Aneg/calendar/internal/repositories/postgres"
 	calendar2 "github.com/Aneg/calendar/internal/services/calendar"
 	"github.com/Aneg/calendar/internal/web"
 	grpc2 "github.com/Aneg/calendar/internal/web/grpc"
 	calendar3 "github.com/Aneg/calendar/pkg/api"
+	"github.com/Aneg/calendar/pkg/db"
 	log2 "github.com/Aneg/calendar/pkg/log"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -40,7 +40,11 @@ func main() {
 	grpcServer := grpc.NewServer()
 	reflection.Register(grpcServer)
 
-	calendarServer := &grpc2.CalendarServer{Calendar: calendar2.NewCalendarService(postgres.NewEventRepository())}
+	pgConn, err := db.PostgresOpenConnection(conf.DBUser, conf.DBPass, conf.DBHost, conf.DBPort, conf.DBName)
+	if err != nil {
+		log2.Logger.Fatal(err.Error())
+	}
+	calendarServer := &grpc2.CalendarServer{Calendar: calendar2.NewCalendarService(postgres.NewEventRepository(pgConn))}
 
 	calendar3.RegisterCalendarServer(grpcServer, calendarServer)
 	grpcServer.Serve(lis)
